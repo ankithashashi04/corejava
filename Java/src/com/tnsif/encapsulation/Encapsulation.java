94172acc72b687799bb34f75281e4134ca5e1578
Ankitha Shashi <Ankitha Shashi@DESKTOP-GQTHSDT>
2024-06-25 18:50:03 +0530

    commit encapsulation

---
+++ Java/src/com/tnsif/encapsulation/Encapsulation.java
@@ -0,0 +1,13 @@
+// Program to demonstrate encapsulation
+package com.tnsif.encapsulation;
+
+public class Encapsulation {
+	int serialNum;//Datavariables
+	String name;
+	int age;
+	void show()
+	{
+		System.out.println("serialnumber"+serialNum+"\t Name"+name+"\t Age:"+age);
+	}
+
+}
---
+++ Java/src/com/tnsif/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,14 @@
+package com.tnsif.encapsulation;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Encapsulation ob= new Encapsulation();
+        ob.name="anki";
+        ob.age=20;
+        ob.serialNum=28;
+        ob.show();
+	}
+
+}
