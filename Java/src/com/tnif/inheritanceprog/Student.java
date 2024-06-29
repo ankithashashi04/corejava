8da5f600f7bf432a446c336cc949551237813a6a
Ankitha Shashi <Ankitha Shashi@DESKTOP-GQTHSDT>
2024-06-29 17:09:08 +0530

    second commit

---
+++ Java/src/com/tnif/inheritanceprog/Student.java
@@ -0,0 +1,24 @@
+package com.tnif.inheritanceprog;
+
+public class Student {
+	private String collageName;
+	private int rollno;
+	public Student()
+	{
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public String getCollageName() {
+		return collageName;
+	}
+	public void setCollageName(String collageName) {
+		this.collageName = collageName;
+	}
+	public int getRollno() {
+		return rollno;
+	}
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+	
+}
---
+++ Java/src/com/tnsif/encapsulation/ScannerDemo.java
@@ -0,0 +1,20 @@
+package com.tnsif.encapsulation;
+
+import java.util.Scanner;
+
+public class ScannerDemo {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+        Scanner ob=new Scanner(System.in);
+        int id=ob.nextInt();
+        if(ob.hasNextInt())
+        {
+        	System.out.println(id);
+        }
+        String name=ob.nextLine();
+        char a=ob.next
+	}
+
+}
